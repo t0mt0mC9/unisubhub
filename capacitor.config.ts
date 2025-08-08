46484d1a3b371c74e8368a97b8fc7df2f8724902
remil50320 <112395757+remil50320@users.noreply.github.com>
2025-08-08 16:33:04 +0200

    Update capacitor.config.ts
    Le capacitor.config.ts est configuré pour pointer vers un serveur distant (https://c6cdb938-7790-42f1-abd3-9729bbdbc721.lovableproject.com) au lieu d'utiliser les fichiers locaux.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.c6cdb938779042f1abd39729bbdbc721',
   appName: 'UniSubHub',
   webDir: 'dist',
-  server: {
-    url: 'https://c6cdb938-7790-42f1-abd3-9729bbdbc721.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  // server: {
+  //   url: 'https://c6cdb938-7790-42f1-abd3-9729bbdbc721.lovableproject.com?forceHideBadge=true',
+  //  cleartext: true
+  // },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
